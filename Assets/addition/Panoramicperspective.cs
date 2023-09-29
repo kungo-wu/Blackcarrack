fef90eeea5cd123c151f3f1ee97b7460f60e0c30
kungo-wu <1807973294@qq.com>
2023-09-30 03:21:40 +0800

    23/9/30 改动
    浮空未发现；
    背包已经清空；
    花园桅杆转场未发现；
    镜头动画完成；
    翘二郎腿未完成；
    船员未触发qte解决；
    位置移动暂时修改；
    鱼错位未发现；

--- Assets/addition/Panoramicperspective.cs
+++ Assets/addition/Panoramicperspective.cs
@@ -9,6 +9,7 @@ public class Panoramicperspective : MonoBehaviour
     public GameObject changetarget;
     private bool changecanmera;
     public GameObject camera;
+    private bool Onmast;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,15 +23,29 @@ public class Panoramicperspective : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Onmast&&SceneManager.GetActiveScene().name=="Deck(Ship)")
+        {
+             if(Input.GetKeyDown(KeyCode.Space))
+            {
+                  Debug.Log("按下空格");
+                 camera.SetActive(!camera.activeInHierarchy);    
+                 
+
+            }
+
+        }
         if(changecanmera&&SceneManager.GetActiveScene().name=="Deck(Ship)")
         {
-            camera.SetActive(true);
+            //camera.SetActive(true);
+           
+           
             leavepositionondeck.SetActive(true);
             changetarget.SetActive(false);
             changecanmera=false;
         }
         else if(SceneManager.GetActiveScene().name!="Deck(Ship)")
         {
+            Onmast=false;
             camera.SetActive(false);
         }
     }
@@ -40,6 +55,7 @@ public class Panoramicperspective : MonoBehaviour
         {
             leavepositionondeck.SetActive(false);
             changetarget.SetActive(true);
+            Onmast=true;
             changecanmera=true;
         }
     }
